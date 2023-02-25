bfd5629d12e4bd22cd8abf27c170ce160861d8af
Dilshad Ahmad <dilshad14@gmail.com>
2023-02-25 20:40:30 +0800

    Initial code check-in

---
+++ backend-svc/src/main/java/com/demo/spring/graceful/shutdown/backend/controller/EmployeeController.java
@@ -0,0 +1,66 @@
+package com.demo.spring.graceful.shutdown.backend.controller;
+
+
+import com.demo.spring.graceful.shutdown.backend.model.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+@RequestMapping("/backend")
+public class EmployeeController {
+
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
+    volatile AtomicInteger counter = new AtomicInteger(0);
+
+    @Value("${app.response.delay.Milis}")
+    Integer appResponseDelayMillis;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployeeList() {
+        return populateEmpList();
+
+    }
+
+    private List<Employee> populateEmpList() {
+        List<Employee> lst = new ArrayList<>();
+        for (int i = 100; i <= 102; i++) {
+            lst.add(new Employee(getEmpId(), UUID.randomUUID()
+                    .toString()));
+        }
+        logger.info("{} - Counter={} - Response={}"
+                , System.getenv("HOSTNAME")
+                , counter.incrementAndGet(), Arrays.toString(lst.toArray()));
+
+        try {
+            Thread.sleep(appResponseDelayMillis);
+        } catch (InterruptedException e) {
+            logger.error("EmployeeController.populateEmpList() - InterruptedException while sleeping");
+        }
+        return lst;
+    }
+
+    public int  getEmpId() {
+        synchronized(this) {
+            SecureRandom r = new SecureRandom();
+            return 10000 + r.nextInt(20000);
+        }
+    }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return " Hi from Backend..!!";
+    }
+
+
+}
